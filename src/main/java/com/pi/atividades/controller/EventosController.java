c47d5be81395932915f2fb66f0c8137356eccfb2
Francisco-Geova <f.geova@escolar.ifrn.edu.br>
2025-12-26 15:16:42 -0300

    Finalizando modificações

--- src/main/java/com/pi/atividades/controller/EventosController.java
+++ src/main/java/com/pi/atividades/controller/EventosController.java
@@ -1,13 +1,18 @@
 package com.pi.atividades.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pi.atividades.models.Eventos;
+import com.pi.atividades.repositories.EventosRepository;
 
 @Controller
 public class EventosController {
+	
+	@Autowired
+	private EventosRepository er;
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
@@ -17,7 +22,8 @@ public class EventosController {
 	@PostMapping("/eventos")
 	public String adicionar(Eventos eventos) {
 		
-		System.out.println(Eventos);
+		System.out.println(eventos);
+		er.save(eventos);
 		return "evento-adicionado";
 	}
 }
